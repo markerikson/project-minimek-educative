44a18c48673bcce1d25bd4c484d8b3f5c5193f88
Mark Erikson <mark@isquaredsoftware.com>
2017-08-23 15:47:07 -0400

    Add initial Redux configuration and sample component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+import './index.css';
+
+import configureStore from "./store/configureStore";
+const store = configureStore();
+
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
