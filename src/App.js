a155664cbed531100ba8051d02c365a14e370951
Mark Erikson <mark@isquaredsoftware.com>
2017-09-14 22:31:22 -0400

    Render initial hardcoded tab bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import {Header} from "semantic-ui-react";
+import {
+    Header,
+    Container,
+    Menu,
+} from "semantic-ui-react";
 
 import './App.css';
 
@@ -10,6 +14,14 @@ class App extends Component {
                 <div className="App-header">
                     <Header inverted as="h1">Project Mini-Mek</Header>
                 </div>
+                <Container>
+                    <Menu tabular>
+                        <Menu.Item name="unitInfo" active={true}>Unit Info</Menu.Item>
+                        <Menu.Item name="pilots" active={false}>Pilots</Menu.Item>
+                        <Menu.Item name="mechs" active={false}>Mechs</Menu.Item>
+                        <Menu.Item name="unitOrganization" active={false}>Unit Organization</Menu.Item>
+                    </Menu>
+                </Container>
             </div>
         );
     }
