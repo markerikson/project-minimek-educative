6660974d17b2f8f815e2d6b5bebd54d46c5d7301
Mark Erikson <mark@isquaredsoftware.com>
2017-10-08 21:05:58 -0400

    Rewrite tabs handling to be driven by Redux
    Added reducer utility functions
    Added tab handling logic
    Added tabs reducer to root reducer
    
    Rewrote TabBarContainer to be a connected version of TabBar

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,9 @@
 import {combineReducers} from "redux";
 
+import tabsReducer from "features/tabs/tabsReducer";
+
 const rootReducer = combineReducers({
+    tabs : tabsReducer
 });
 
 export default rootReducer;
