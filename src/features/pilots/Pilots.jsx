1f17b50c83171e0807d3bb487560d972f02a8798
Mark Erikson <mark@isquaredsoftware.com>
2017-10-22 16:31:36 -0700

    Connect the Pilots component to render a list of pilots from the store

--- src/features/pilots/Pilots.jsx
+++ src/features/pilots/Pilots.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
 
 import {
     Grid,
@@ -6,28 +7,39 @@ import {
     Header,
 } from "semantic-ui-react";
 
+import orm from "app/orm";
 
 import PilotsList from "./PilotsList";
 import PilotDetails from "./PilotDetails";
 
-const pilots = [
-    {
-        name : "Natasha Kerensky",
-        rank : "Captain",
-        age : 52,
-        gunnery : 2,
-        piloting : 3,
-        mechType : "WHM-6R",
-    }
-];
 
-class Pilots extends Component {
-    state = {
-        pilots : pilots,
-    }
+const mapState = (state) => {
+    // Create a Redux-ORM Session from our "entities" slice, which
+    // contains the "tables" for each model type
+    const session =  orm.session(state.entities);
 
+    // Retrieve the model class that we need.  Each Session
+    // specifically "binds" model classes to itself, so that
+    // updates to model instances are applied to that session.
+    // These "bound classes" are available as fields in the sesssion.
+    const {Pilot} = session;
+
+    // Query the session for all Pilot instances.
+    // The QuerySet that is returned from all() can be used to
+    // retrieve instances of the Pilot class, or retrieve the
+    // plain JS objects that are actually in the store.
+    // The toRefArray() method will give us an array of the
+    // plain JS objects for each item in the QuerySet.
+    const pilots = Pilot.all().toRefArray();
+
+    // Now that we have an array of all pilot objects, return it as a prop
+    return {pilots};
+}
+
+
+export class Pilots extends Component {
     render() {
-        const {pilots} = this.state;
+        const {pilots = []} = this.props;
 
         // Use the first pilot as the "current" one for display, if available.
         const currentPilot = pilots[0] || {};
@@ -51,4 +63,4 @@ class Pilots extends Component {
     }
 }
 
-export default Pilots
+export default connect(mapState)(Pilots);
