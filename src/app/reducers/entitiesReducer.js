1825918c387d6076e6ef605c429772ea97b149b3
Mark Erikson <mark@isquaredsoftware.com>
2017-10-22 15:51:12 -0700

    Add Pilot parsing, and load Pilots from sample data

--- src/app/reducers/entitiesReducer.js
+++ src/app/reducers/entitiesReducer.js
@@ -2,7 +2,25 @@ import {createReducer} from "common/utils/reducerUtils";
 
 import orm from "app/orm"
 
+import {DATA_LOADED} from "features/tools/toolConstants";
+
+
 const initialState = orm.getEmptyState()
 
+export function loadData(state, payload) {
+    // Create a Redux-ORM session from our entities "database tables" object
+    const session = orm.session(state);
+    // Get a reference to the correct version of the Pilots class for this Session
+    const {Pilot} = session;
+
+    const {pilots} = payload;
+    // Insert the Pilot entries into the Session
+    pilots.forEach(pilot => Pilot.parse(pilot));
+
+    // return a new version of the entities state object with the inserted entries
+    return session.state;
+}
+
 export default createReducer(initialState, {
+    [DATA_LOADED] : loadData
 });
--- src/features/pilots/Pilot.js
+++ src/features/pilots/Pilot.js
@@ -3,4 +3,14 @@ import {Model} from "redux-orm";
 
 export default class Pilot extends Model {
     static modelName = "Pilot";
+
+    static parse(pilotData) {
+        // We could do useful stuff in here with relations,
+        // but since we have no relations yet, all we need
+        // to do is pass the pilot data on to create()
+
+        // Note that in a static class method, `this` is the
+        // class itself, not an instance
+        return this.create(pilotData);
+    }
 }
