dca21ea47b2b30196328540f9cbe78c8200558ae
Mark Erikson <mark@isquaredsoftware.com>
2017-10-22 22:57:45 -0700

    Load Mechs and MechDesigns from sample data
    Added Mech and MechDesign entries to the sample data
    Added reducer logic to load them into the store

--- src/app/reducers/entitiesReducer.js
+++ src/app/reducers/entitiesReducer.js
@@ -10,12 +10,15 @@ const initialState = orm.getEmptyState()
 export function loadData(state, payload) {
     // Create a Redux-ORM session from our entities "database tables" object
     const session = orm.session(state);
-    // Get a reference to the correct version of the Pilots class for this Session
-    const {Pilot} = session;
+    // Get a reference to the correct version of model classes for this Session
+    const {Pilot, MechDesign, Mech} = session;
 
-    const {pilots} = payload;
-    // Insert the Pilot entries into the Session
+    const {pilots, designs, mechs} = payload;
+
+    // Insert the data entries into the Session
     pilots.forEach(pilot => Pilot.parse(pilot));
+    designs.forEach(design => MechDesign.parse(design));
+    mechs.forEach(mech => Mech.parse(mech));
 
     // return a new version of the entities state object with the inserted entries
     return session.state;
--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -112,6 +112,115 @@ const sampleData = {
             age : 29,
             mechType : "STG-3R",
         },
+    ],
+    designs : [
+        {
+            id : "STG-3R",
+            name : "Stinger",
+            weight : 20,
+        },
+        {
+            id : "WSP-1A",
+            name : "Wasp",
+            weight : 20,
+        },
+        {
+            id : "PXH-1K",
+            name : "Phoenix Hawk",
+            weight : 45,
+        },
+        {
+            id : "GRF-1N",
+            name : "Griffin",
+            weight : 55,
+        },
+        {
+            id : "RFL-3N",
+            name : "Rifleman",
+            weight : 60,
+        },
+        {
+            id : "CRD-3R",
+            name : "Crusader",
+            weight : 65,
+        },
+        {
+            id : "ARC-2R",
+            name : "Archer",
+            weight : 70,
+        },
+        {
+            id : "WHM-6R",
+            name : "Warhammer",
+            weight : 70,
+        },
+        {
+            id : "MAD-3R",
+            name : "Marauder",
+            weight : 75,
+        },
+    ],
+    mechs : [
+        {
+            id : 1,
+            type : "WHM-6R",
+            pilot : 1,
+        },
+        {
+            id : 2,
+            type : "MAD-3R",
+            pilot : 2,
+        },
+        {
+            id : 3,
+            type : "CRD-3R",
+            pilot : 3,
+        },
+        {
+            id : 4,
+            type : "GRF-1N",
+            pilot : 4,
+        },
+        {
+            id : 5,
+            type : "ARC-2R",
+            pilot : 5,
+        },
+        {
+            id : 6,
+            type : "ARC-2R",
+            pilot : 6,
+        },
+        {
+            id : 7,
+            type : "WSP-1A",
+            pilot : 7,
+        },
+        {
+            id : 8,
+            type : "STG-3R",
+            pilot : 8,
+        },
+        {
+            id : 9,
+            type : "RFL-3N",
+            pilot : 9,
+        },
+        {
+            id : 10,
+            type : "PXH-1K",
+            pilot : 10,
+        },
+        {
+            id : 11,
+            type : "STG-3R",
+            pilot : 11,
+        },
+        {
+            id : 12,
+            type : "STG-3R",
+            pilot : 12,
+        }
     ]
 };
 
