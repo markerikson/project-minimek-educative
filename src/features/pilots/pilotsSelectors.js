ed3c4606b04973a35824a0541b5fc92d7e1aadf1
Mark Erikson <mark@isquaredsoftware.com>
2017-11-04 18:13:15 -0400

    Add logic to track if a pilot is being edited

--- src/features/pilots/pilotsSelectors.js
+++ src/features/pilots/pilotsSelectors.js
@@ -6,3 +6,9 @@ export const selectCurrentPilot = createSelector(
     selectPilots,
     pilots => pilots.currentPilot,
 );
+
+
+export const selectIsEditingPilot = createSelector(
+    selectPilots,
+    pilots => pilots.isEditing
+);
