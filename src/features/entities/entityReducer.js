779dbdc65caa7187391ba7da1f688b2fabe33b7a
Mark Erikson <mark@isquaredsoftware.com>
2017-11-07 21:07:17 -0500

    Add generic entity creation and deletion handling

--- src/features/entities/entityReducer.js
+++ src/features/entities/entityReducer.js
@@ -1,4 +1,8 @@
-import {ENTITY_UPDATE} from "./entityConstants";
+import {
+    ENTITY_UPDATE,
+    ENTITY_DELETE,
+    ENTITY_CREATE,
+} from "./entityConstants";
 
 import {createConditionalSliceReducer} from "common/utils/reducerUtils";
 
@@ -23,8 +27,41 @@ export function updateEntity(state, payload) {
     return newState;
 }
 
+export function deleteEntity(state, payload) {
+    const {itemID, itemType} = payload;
+
+    const session = orm.session(state);
+    const ModelClass = session[itemType];
+
+    let newState = state;
+
+    if(ModelClass.hasId(itemID)) {
+        const modelInstance = ModelClass.withId(itemID);
+
+        modelInstance.delete();
+
+        newState = session.state
+    }
+
+    return newState;
+}
+
+export function createEntity(state, payload) {
+    const {itemType, newItemAttributes} = payload;
+
+    const session = orm.session(state);
+    const ModelClass = session[itemType];
+
+    ModelClass.parse(newItemAttributes);
+
+    return session.state;
+}
+
+
 const entityHandlers = {
     [ENTITY_UPDATE] : updateEntity,
+    [ENTITY_CREATE] : createEntity,
+    [ENTITY_DELETE] : deleteEntity,
 };
 
 const entityCrudFeatureReducer = createConditionalSliceReducer("entities", entityHandlers);
