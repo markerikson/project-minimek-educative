f20378c299fa9d821cde6b38b6b6838279b8f2de
Mark Erikson <mark@isquaredsoftware.com>
2017-11-11 17:02:41 -0500

    Implement stackable modals

--- src/features/modals/modalReducer.js
+++ src/features/modals/modalReducer.js
@@ -1,20 +1,27 @@
-import {createReducer} from "common/utils/reducerUtils";
-
 import {
     MODAL_CLOSE,
     MODAL_OPEN
 } from "./modalConstants";
 
-const initialState = null;
+import {createReducer} from "common/utils/reducerUtils";
+
+
+const initialState = [];
+
 
 export function openModal(state, payload) {
     const {modalType, modalProps} = payload;
-    return {modalType, modalProps};
+    // Always pushing a new modal onto the stack
+    return state.concat({modalType, modalProps});
 }
 
 export function closeModal(state, payload) {
-    return null;
+    // Always popping the last modal off the stack
+    const newState = state.slice();
+    newState.pop();
+    return newState;
 }
+
 
 export default createReducer(initialState,  {
     [MODAL_OPEN] : openModal,
